b6b751e0b7c51217a96354cdcca530a5834f2a2a
jinyoungTest15 <jinyoung010402@gmail.com>
2025-04-28 15:25:35 +0900

    Test: "로그아웃기능 테스트"
    로그아웃 기능 테스트 코드를 구현했습니다. ☔️

--- demo/src/test/java/com/kh/demo/member/MemberTest.java
+++ demo/src/test/java/com/kh/demo/member/MemberTest.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.kh.demo.dao.MemberDao;
+import com.kh.demo.dao.TokenDao;
 import com.kh.demo.dto.MemberDto;
+import com.kh.demo.dto.TokenDto;
 import com.kh.demo.service.MemberService;
+import com.kh.demo.service.TokenService;
 
 import jakarta.mail.MessagingException;
 import lombok.extern.slf4j.Slf4j;
@@ -16,22 +19,28 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @SpringBootTest
 public class MemberTest {
-	
+
 	@Autowired
 	private MemberService memberService;
-	
+
 	@Autowired
 	private MemberDao memberDao;
-	
+
+	@Autowired
+	private TokenDao tokenDao;
+
+	@Autowired
+	private TokenService tokenService;
+
 	@Test
 	public void test() throws MessagingException, IOException {
 		// 회원가입 테스트
 //		memberService.signup(
 //			MemberDto.builder()
 //			.memberNo(memberDao.sequence())
-//			.memberId("testuser1")
-//			.memberPw("fdskjbfdjksbjkfdsbkjdskjbk")
-//			.memberNickname("테스트유저1")
+//			.memberId("testuser11")
+//			.memberPw("Testuser11!")
+//			.memberNickname("홍길동11")ㄴ
 //			.memberEmail("yno1109@naver.com")
 //			.memberLocation("경기도 성남시")
 //			.memberSchool("용인대학교")
@@ -41,7 +50,7 @@ public class MemberTest {
 //			.memberMbti("ISTJ")
 //			.build()
 //		);
-		
+
 		// 로그인 테스트
 //		log.debug("member = {}", 
 //				memberService.signin(
@@ -50,11 +59,31 @@ public class MemberTest {
 //						.memberPw("fdskjbfdjksbjkfdsbkjdskjbk")
 //					.build()
 //				));
-		
+
 		// 아이디 찾기 테스트
 //		log.debug("memberId = {}", memberDao.findId("demo@mail.com"));
-		
+
 		// 비밀번호 찾기 테스트
 //		memberService.sendTempPassword("yno1109@naver.com");
+
+		// 로그아웃 테스트
+//		String memberId = "testuser1";
+//
+//	    // Bearer 없이 순수 accessToken만!
+//	    String accessToken = tokenService.generateAccessToken(memberId);
+//
+//	    // 파싱
+//	    String extractedMemberId = tokenService.parse(accessToken);
+//
+//	    // 로그아웃 처리
+//	    tokenDao.clean(extractedMemberId);
+//
+//	    TokenDto tokenDto = TokenDto.builder()
+//	        .tokenTarget(extractedMemberId)
+//	        .build();
+//
+//	    TokenDto findToken = tokenDao.find(tokenDto);
+//
+//	    System.out.println("findToken = " + findToken);
 	}
 }
