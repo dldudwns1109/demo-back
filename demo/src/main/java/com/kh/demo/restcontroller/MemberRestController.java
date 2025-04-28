c6cac6237cd973096c68dd1660a458599296186d
jinyoungTest15 <jinyoung010402@gmail.com>
2025-04-28 15:19:22 +0900

    Feat: "추가 로그아웃 기능"
    로그아웃 기능을 구현했습니다. ✨

--- demo/src/main/java/com/kh/demo/restcontroller/MemberRestController.java
+++ demo/src/main/java/com/kh/demo/restcontroller/MemberRestController.java
@@ -5,16 +5,20 @@ import java.io.IOException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.demo.dao.MemberDao;
+import com.kh.demo.dao.TokenDao;
 import com.kh.demo.dto.MemberDto;
+import com.kh.demo.error.TargetNotFoundException;
 import com.kh.demo.service.MemberService;
 import com.kh.demo.service.TokenService;
 import com.kh.demo.vo.MemberSigninRequestVO;
@@ -35,6 +39,9 @@ public class MemberRestController {
 	
 	@Autowired
 	private MemberDao memberDao;
+	
+	@Autowired
+	private TokenDao tokenDao;
 	
 	@PostMapping("/signup")
 	public void signup(@RequestBody MemberDto memberDto) {
@@ -57,7 +64,7 @@ public class MemberRestController {
 	}
 	
 	@PostMapping("/refresh")
-	public MemberSigninResponseVO refresh(@RequestBody String refreshToken) {
+	public MemberSigninResponseVO refresh(@RequestHeader("Authorization") String refreshToken) {
 		String memberId = tokenService.parseBearerToken(refreshToken);
 		
 		if (!tokenService.checkBearerToken(memberId, refreshToken)) 
@@ -68,6 +75,12 @@ public class MemberRestController {
 									.accessToken(tokenService.generateAccessToken(memberId))
 									.refreshToken(tokenService.generateRefreshToken(memberId))
 									.build();
+	}
+	
+	@PostMapping("/signout")
+	 public void logout(@RequestHeader("Authorization") String accessToken) {
+		String memberId = tokenService.parse(accessToken);
+	    tokenDao.clean(memberId);
 	}
 	
 	@GetMapping("/memberEmail/{memberEmail}")
