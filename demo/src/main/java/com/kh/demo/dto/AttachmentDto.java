54f4121074d39c4328a111be82d8d5fd25ac0814
jinyoungTest15 <jinyoung010402@gmail.com>
2025-04-29 16:05:49 +0900

    Feat: "추가 Attachment 기능"
    AttachmentDto, attachment-mapper, AttachmentDao, AttachmentService,
    FileuploadProperties, AttachmentRestController 설정하여 파일 업로드
    파일 상세조회 파일 삭제 기능 추가했습니다. ✨

---
+++ demo/src/main/java/com/kh/demo/dto/AttachmentDto.java
@@ -0,0 +1,14 @@
+package com.kh.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
+public class AttachmentDto {
+	private long attachmentNo;//시퀀스번호
+	private String attachmentName;//업로드한 파일명
+	private String attachmentType;//업로드한 파일유형
+	private long attachmentSize;//업로드한 파일크기
+}
