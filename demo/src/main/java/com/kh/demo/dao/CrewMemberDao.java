529889d9efcc2f65e94cae28036b3892065671f3
kwkgsjd <kwkgsjd0410@gmail.com>
2025-04-30 17:35:06 +0900

    Feat : "모임 관련 백엔드 구현(crew,crewMember)"
    가입/탈퇴, 모임장 권한 확인 및 강퇴 기능 포함한 모임관련 백엔드를 구현하였습니다. ✨

---
+++ demo/src/main/java/com/kh/demo/dao/CrewMemberDao.java
@@ -0,0 +1,47 @@
+package com.kh.demo.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.demo.dto.CrewMemberDto;
+import com.kh.demo.vo.CrewMemberVO;
+
+@Repository
+public class CrewMemberDao {
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	// 모임 가입 처리
+    public void join(CrewMemberDto crewMemberDto) {
+        sqlSession.insert("crewmember.join", crewMemberDto);
+    }
+
+    // 모임 탈퇴 처리
+    public boolean leave(CrewMemberDto crewMemberDto) {
+        return sqlSession.delete("crewmember.leave", crewMemberDto) > 0;
+    }
+
+    // 해당 사용자가 모임장인지 확인
+    public boolean isLeader(CrewMemberDto crewMemberDto) {
+        return sqlSession.selectOne("crewmember.isLeader", crewMemberDto);
+    }
+
+    // 모임 가입 여부 확인
+    public boolean isMember(CrewMemberDto crewMemberDto) {
+        return sqlSession.selectOne("crewmember.isMember", crewMemberDto);
+    }
+
+    // 특정 모임의 전체 회원 목록
+    public List<CrewMemberVO> selectListByCrew(Long crewNo) {
+        return sqlSession.selectList("crewmember.selectListByCrew", crewNo);
+    }
+
+    // 모임장만 가능한 회원 강퇴
+    public boolean kick(CrewMemberDto crewMemberDto) {
+        return sqlSession.delete("crewmember.kick", crewMemberDto) > 0;
+    }
+}
