1937b1e61db235b528f8e59abe631a6cfd20a5f9
jinyoungTest15 <jinyoung010402@gmail.com>
2025-05-01 16:22:05 +0900

    Feat: "추가 결제 Service, RestController"
    PayService, PayRestController, CrewPayRequestVO 구현 했습니다 ✨

---
+++ demo/src/main/java/com/kh/demo/vo/CrewPayRequestVO.java
@@ -0,0 +1,14 @@
+package com.kh.demo.vo;
+
+import com.kh.demo.dto.CrewDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
+public class CrewPayRequestVO {
+    private CrewDto crewDto;        // 모임 정보
+    private int totalAmount;     // 결제 금액
+}
